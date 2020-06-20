9963aa6ece1ab5be53d8b2841222087a58206f93
wyu <kharma@gmail.com>
2020-06-20 15:15:27 -0700

    date plot1

--- plot1.R
+++ plot1.R
@@ -4,6 +4,15 @@
 # Name: Walter Yu
 # Date: June 2020
 
+setwd("C:/Users/kharm/Documents/R/jhu_dsci/04_eda/ExData_Plotting1")
+
 # Notes: 
 # 1. all code included to show data import, plot and export to png
-read.table("")
+
+# source: https://bookdown.org/rdpeng/rprogdatascience/getting-data-in-and-out-of-r.html
+# rows_10 <- read.table("household_power_consumption.txt", nrows=10)
+# classes  <- sapply(rows_10, class)
+# print(classes)
+
+# source: https://bookdown.org/rdpeng/rprogdatascience/getting-data-in-and-out-of-r.html
+data <- read.table("household_power_consumption.txt")
