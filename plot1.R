444cf5d95b04ae48afee39bedfbd991ca4e0d338
wyu <kharma@gmail.com>
2020-06-20 15:00:39 -0700

    add plot1.R

---
+++ plot1.R
@@ -0,0 +1,9 @@
+# JHU-DSCI EDA
+# Assignment: Project 1
+# Filename: plot1.R
+# Name: Walter Yu
+# Date: June 2020
+
+# Notes: 
+# 1. all code included to show data import, plot and export to png
+read.table("")
